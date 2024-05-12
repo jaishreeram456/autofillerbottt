eae8bbaa100d9efd65a9dd0ae117af370d996fc4
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-05-12 07:11:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot 
+  git clone https://github.com/The-Happy-Hour/silicon /silicon
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMoviesProviderBot
+  git clone $UPSTREAM_REPO /silicon
 fi
 cd /THALAPATHY-FILTER-BOT 
 pip3 install -U -r requirements.txt
